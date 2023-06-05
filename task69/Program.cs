e02a65a6ea805f0e83c38cbebc3f681fc0377474
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-05 23:19:54 +0300

    Added info about task 69

---
+++ task69/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите количество смываний говна:");  
+int N = Convert.ToInt32(Console.ReadLine());                                                                                                               
+if  (N < 10)                                                            
+{
+Console.WriteLine ("Не смоется говно");                                                                        
+}
+if (N >= 10)   
+{
+Console.WriteLine ("Миссия выполнена");                                                         
+}
